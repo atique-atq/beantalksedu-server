4bcc12e8a7983ca8fc35558f2a42ad0130bbec69
atique-atq <atique.atq@gmail.com>
2023-02-01 18:54:49 +0600

    added utilities function

--- index.js
+++ index.js
@@ -4,63 +4,53 @@ const app = express();
 const port = process.env.PORT || 5000;
 const multer  = require('multer');
 const csv = require("csvtojson");
+const utilities = require("./utilities");
 
 // middleware
 app.use(cors());
 app.use(express.json());
 const upload = multer({ storage: multer.memoryStorage() })
 
+//log level filter
 const logLevel = ['warn', 'error'];
 
-app.get("/", async (req, res) => {
-  res.send("beansTalkEdu is running");
-});
 
 async function run() {
   try {
-    //process log
+    //process log api
     app.post("/processlog", upload.single('file'),  (req, res) => {
-      const logArray =  (req.file.buffer.toString()).split(/\r?\n/);
-
-      // console.log('Before filtered array: ', logArray);
-    
-        if (logArray.length === 0) {
-          return res.send({ errorData: 'invalid file' });
-        }
-        
-        let filteredLogArray = logArray.filter(element => { 
-            return logLevel.includes(element.split(' - ')[1].toLowerCase())
-        });
-        
-        let expectedArray = filteredLogArray.map(element => {
+      const logArray =  req.file.buffer.toString().split(/\r?\n/);
+      if (logArray[0].length === 0) {
+        return res.status(utilities.invalidContentErrorCode).send(utilities.errorFileMessage);
+      }
+      let filteredLogArray = logArray.filter(element => { 
+        return logLevel.includes(element.split(' - ')[1]?.toLowerCase())
+      });
+     
+      let expectedArray = filteredLogArray.map(element => {
           let [timePortion, logStatusPortion, otherDetails] = element.split(' - ')
           let stringTimeValue = timePortion.substring(1);
           let formattedTimeStamp = Math.floor(new Date(stringTimeValue).valueOf());
           let formattedOtherDetails = otherDetails.replaceAll('""', '"').slice(0, -1);
           let detailsJson = JSON.parse(formattedOtherDetails);
-          console.log('json details ', detailsJson.transactionId);
 
-          let expectedValue = {
+          return {
             "timestamp": formattedTimeStamp,
             "loglevel": logStatusPortion,
             "transactionId": detailsJson.transactionId,
             "err": detailsJson.err
           }
-          console.log(expectedValue);
-          return expectedValue;
         });
-      
-        console.log('Final value', expectedArray);
-
-        res.send({data: expectedArray});
+        res.status(utilities.successCode).send({data: expectedArray});
       });
-
   } 
-  
   finally {
-
   }
 }
 run().catch(error => console.error({errorData: error}));
+
+app.get("/", async (req, res) => {
+  res.send("beansTalkEdu task server is running");
+});
 
 app.listen(port, () => console.log(`beansTalkEdu server is running on ${port}`));
---
+++ utilities.js
@@ -0,0 +1,7 @@
+const successCode = 200
+const invalidContentErrorCode = 422
+const errorFileMessage = { data: 'invalid file format' }
+
+module.exports.successCode = successCode;
+module.exports.invalidContentErrorCode = invalidContentErrorCode;
+module.exports.errorFileMessage = errorFileMessage;
